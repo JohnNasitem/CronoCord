64bc4c9d18c6f1f25f77738c9db5d5ac4555b631
JohnNasitem <jnasitem1@nait.ca>
2025-02-18 12:15:13 -0700

    added event class and basic command

---
+++ CronoCord/Modules/EventsModule.cs
@@ -0,0 +1,26 @@
+﻿using Discord.Interactions;
+using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CronoCord.Modules
+{
+    public class EventsModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EventsModule"/>
+        /// </summary>
+        public EventsModule() { }
+
+
+        [SlashCommand("create event", "Create an event")]
+        [DefaultMemberPermissionsAttribute(GuildPermission.ManageEvents)]
+        public async Task CreateEvent()
+        {
+            await RespondAsync($"Creating event...");
+        }
+    }
+}
