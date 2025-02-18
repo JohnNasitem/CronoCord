52e91b2fcf09753de373abe5d0a8d12afe7668a6
JohnNasitem <jnasitem1@nait.ca>
2025-02-18 01:53:51 -0700

    added interaction handler

---
+++ CronoCord/InteractionHandler.cs
@@ -0,0 +1,112 @@
+﻿//***********************************************************************************
+//Program: InteractionHandler.cs
+//Description: Interaction Handler from the discord.net repo
+//Src: https://github.com/discord-net/Discord.Net/blob/dev/samples/InteractionFramework/InteractionHandler.cs
+//Date: Feb 18, 2025
+//Author: John Nasitem
+//***********************************************************************************
+
+using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace CronoCord
+{
+    public class InteractionHandler
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly InteractionService _handler;
+        private readonly IServiceProvider _services;
+
+        public InteractionHandler(DiscordSocketClient client, InteractionService handler, IServiceProvider services)
+        {
+            _client = client;
+            _handler = handler;
+            _services = services;
+        }
+
+        public async Task InitializeAsync()
+        {
+            // Process when the client is ready, so we can register our commands.
+            _client.Ready += ReadyAsync;
+            _handler.Log += LogAsync;
+
+            // Add the public modules that inherit InteractionModuleBase<T> to the InteractionService
+            await _handler.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+
+            // Process the InteractionCreated payloads to execute Interactions commands
+            _client.InteractionCreated += HandleInteraction;
+
+            // Also process the result of the command execution.
+            _handler.InteractionExecuted += HandleInteractionExecute;
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log);
+            return Task.CompletedTask;
+        }
+
+        private async Task ReadyAsync()
+        {
+            // Register the commands globally.
+            // alternatively you can use _handler.RegisterCommandsGloballyAsync() to register commands to a specific guild.
+            await _handler.RegisterCommandsGloballyAsync();
+
+            Console.WriteLine($"{_client.CurrentUser} is connected!");
+        }
+
+        private async Task HandleInteraction(SocketInteraction interaction)
+        {
+            try
+            {
+                // Create an execution context that matches the generic type parameter of your InteractionModuleBase<T> modules.
+                var context = new SocketInteractionContext(_client, interaction);
+
+                // Execute the incoming command.
+                var result = await _handler.ExecuteCommandAsync(context, _services);
+
+                // Due to async nature of InteractionFramework, the result here may always be success.
+                // That's why we also need to handle the InteractionExecuted event.
+                if (!result.IsSuccess)
+                    switch (result.Error)
+                    {
+                        case InteractionCommandError.UnmetPrecondition:
+                            // Handle the case where the precondition for the command is not met
+                            // This can be due to user permissions, missing arguments, or other conditions
+                            await interaction.RespondAsync("You do not meet the preconditions to run this command.");
+                            break;
+                        default:
+                            break;
+                    }
+            }
+            catch
+            {
+                // If Slash Command execution fails it is most likely that the original interaction acknowledgement will persist. It is a good idea to delete the original
+                // response, or at least let the user know that something went wrong during the command execution.
+                if (interaction.Type is InteractionType.ApplicationCommand)
+                    await interaction.GetOriginalResponseAsync().ContinueWith(async (msg) => await msg.Result.DeleteAsync());
+            }
+        }
+
+        private Task HandleInteractionExecute(ICommandInfo commandInfo, IInteractionContext context, IResult result)
+        {
+            if (!result.IsSuccess)
+                switch (result.Error)
+                {
+                    case InteractionCommandError.UnmetPrecondition:
+                        // Handle the case where the precondition for the command is not met
+                        // This can be due to user permissions, missing arguments, or other conditions
+                        context.Interaction.RespondAsync("You do not meet the preconditions to execute this command.");
+                        break;
+                    default:
+                        break;
+                }
+
+            return Task.CompletedTask;
+        }
+    }
+}
