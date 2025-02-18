dd77eeba85630340f48a4c299d61f0ae395b7df1
JohnNasitem <jnasitem1@nait.ca>
2025-02-18 12:54:07 -0700

    organized using statements

--- CronoCord/Modules/UtilityModule.cs
+++ CronoCord/Modules/UtilityModule.cs
@@ -11,10 +11,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Discord.WebSocket;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Interactions;
-using Discord.WebSocket;
 
 namespace CronoCord.Modules
 {
