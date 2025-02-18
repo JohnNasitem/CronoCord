9c3f6c468dde9664bd100914a83e6fa1ece659e1
JohnNasitem <jnasitem1@nait.ca>
2025-02-18 15:26:23 -0700

    moved CreateEventModal class to its own file

--- CronoCord/Modules/EventsModule.cs
+++ CronoCord/Modules/EventsModule.cs
@@ -31,44 +31,7 @@ namespace CronoCord.Modules
         [DefaultMemberPermissionsAttribute(GuildPermission.ManageEvents)]
         public async Task CreateEvent()
         {
-            await RespondWithModalAsync(new CreateEventModal().Build());
-        }
-    }
-
-    public class CreateEventModal : ModalBuilder
-    {
-        public CreateEventModal()
-        {
-            Title = "Create Event";
-            CustomId = "create_event";
-            AddTextInput(label:"Event Name:",
-                         customId: "event_name",
-                         style: TextInputStyle.Short,
-                         required: true);
-            AddTextInput(label: "Event Description:",
-                         customId: "event_desc",
-                         style: TextInputStyle.Paragraph,
-                         required: true);
-            AddTextInput(label: "Start Date:",
-                         customId: "event_start_date",
-                         style: TextInputStyle.Short,
-                         required: true);
-            AddTextInput(label: "End Date:",
-                         customId: "event_end_date",
-                         style: TextInputStyle.Short,
-                         required: true);
-        }
-
-        public static async Task ModelSubmit(SocketModal modal)
-        {
-            // Extract values from the modal's inputs
-            List<SocketMessageComponentData> components = modal.Data.Components.ToList();
-            string name = components.First(x => x.CustomId == "event_name").Value;
-            string desc = components.First(x => x.CustomId == "event_desc").Value;
-            string startDate = components.First(x => x.CustomId == "event_start_date").Value;
-            string endDate = components.First(x => x.CustomId == "event_end_date").Value;
-
-            await modal.RespondAsync($"Name: {name}\nDescription: {desc}\nStart Date: {startDate}\nEnd Date: {endDate}");
+            await RespondWithModalAsync(new Interactions.Modals.CreateEventModal().Build());
         }
     }
 }
