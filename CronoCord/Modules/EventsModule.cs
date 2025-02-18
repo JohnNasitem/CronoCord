42e303e4f35fc124725f3336b6829ed9f70d8182
JohnNasitem <jnasitem1@nait.ca>
2025-02-18 12:55:12 -0700

    fixed problem with command name

--- CronoCord/Modules/EventsModule.cs
+++ CronoCord/Modules/EventsModule.cs
@@ -1,9 +1,9 @@
-﻿using Discord.Interactions;
-using Discord;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Discord.Interactions;
+using Discord;
 using System.Threading.Tasks;
 
 namespace CronoCord.Modules
@@ -16,7 +16,7 @@ namespace CronoCord.Modules
         public EventsModule() { }
 
 
-        [SlashCommand("create event", "Create an event")]
+        [SlashCommand("create-event", "Create an event")]
         [DefaultMemberPermissionsAttribute(GuildPermission.ManageEvents)]
         public async Task CreateEvent()
         {
