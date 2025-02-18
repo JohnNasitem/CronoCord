3f77bb3e857f89091bd3e34cbe9531945e35baa8
JohnNasitem <jnasitem1@nait.ca>
2025-02-18 02:16:06 -0700

    commented code

--- CronoCord/Modules/UtilityModule.cs
+++ CronoCord/Modules/UtilityModule.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿//***********************************************************************************
+//Program: UtilityModule.cs
+//Description: Holds ultility commands that arent for a specific feature
+//Date: Feb 18, 2025
+//Author: John Nasitem
+//***********************************************************************************
+
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,17 +18,31 @@ using Discord.WebSocket;
 
 namespace CronoCord.Modules
 {
-    public class EchoCommand : InteractionModuleBase<SocketInteractionContext>
+    public class UtilityModule : InteractionModuleBase<SocketInteractionContext>
     {
-        public EchoCommand() { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UtilityModule"/>
+        /// </summary>
+        public UtilityModule() { }
 
+
+
+        /// <summary>
+        /// Ping slash command
+        /// </summary>
+        /// <returns>Task</returns>
         [SlashCommand("ping", "Get bot latency")]
-        public async Task Ping()
-        => await RespondAsync($"Pong! Latency: {Context.Client.Latency} ms");
+        public async Task Ping() =>
+            await RespondAsync($"Pong! Latency: {Context.Client.Latency} ms");
 
+
+
+        /// <summary>
+        /// Info slash command
+        /// </summary>
+        /// <returns>Task</returns>
         [SlashCommand("info", "Get bot info")]
-        public async Task Info()
-        {
+        public async Task Info() =>
             await RespondAsync(embed: new EmbedBuilder()
                                     .WithTitle("Bot Information")
                                     .WithDescription("This is a bot that manages schedules as well a events")
@@ -27,6 +50,6 @@ namespace CronoCord.Modules
                                     .AddField("Author", Program.Author, inline: true)
                                     .AddField("Version", Program.Version, inline: true)
                                     .Build());
-        }
+
     }
 }
