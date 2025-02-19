752fd35f7c70e934aa20998a8e41e16a55a33e01
JohnNasitem <jnasitem1@nait.ca>
2025-02-19 02:11:58 -0700

    got basic image to disable

--- CronoCord/Modules/AvailabilityModule.cs
+++ CronoCord/Modules/AvailabilityModule.cs
@@ -10,7 +10,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Discord.Interactions;
 using Discord;
 using System.Threading.Tasks;
@@ -19,11 +18,9 @@ using System.Reflection;
 using CronoCord.Utilities;
 using CronoCord.Classes;
 using System.Text.RegularExpressions;
-using SkiaSharp;
 using System.Windows;
 using System.IO;
-using static System.Net.Mime.MediaTypeNames;
-
+using System.Drawing;
 
 namespace CronoCord.Modules
 {
@@ -100,7 +97,7 @@ namespace CronoCord.Modules
         private async Task ViewSchedule(List<IUser> users, int weekOffset, bool showOverlapCount)
         {
             List<Availability> unfiltered_availabilities = null;
-            List<Availability> filtered_availabilities = null;
+            List<Availability> filtered_availabilities = new List<Availability>();
 
             Console.WriteLine("ViewSchedule - 1");
             await Task.Run(() => unfiltered_availabilities = DatabaseManagement.GetAvailabilites(users.Select(u => u.Id).ToList()));
@@ -119,7 +116,7 @@ namespace CronoCord.Modules
             foreach (Availability availability in unfiltered_availabilities)
             {
                 // Include the availabilities happning this week
-                if (UtilityMethods.GetSundayUnixTimeStamp(false) <= availability.StartTimeUnix && availability.StartTimeUnix <= UtilityMethods.GetSundayUnixTimeStamp(true))
+                if (UtilityMethods.GetSundayUnixTimeStamp(true) <= availability.StartTimeUnix && availability.StartTimeUnix < UtilityMethods.GetSundayUnixTimeStamp(false))
                     filtered_availabilities.Add(availability);
                 // Including repeating availabilities
                 //else if (availability.IsRecurring != Availability.Recurring.N)
@@ -157,29 +154,58 @@ namespace CronoCord.Modules
             // TODO: this breaks when availabilities is empty
             List<ulong> uniqueIDs = availabilities.Select(a => a.UserID).Distinct().ToList();
             Console.WriteLine("GenerateScheduleImage - 2");
-            Dictionary<ulong, SKPaint> userColours = GenerateUserColours(uniqueIDs);
+            Dictionary<ulong, System.Drawing.Color> userColours = GenerateUserColours(uniqueIDs);
             Console.WriteLine("GenerateScheduleImage - 3");
             //Does it need to be sorted?
             availabilities.Sort((a1, a2) => a1.StartTimeUnix.CompareTo(a2.StartTimeUnix));
             Console.WriteLine("GenerateScheduleImage - 4");
             int backgroundWidth = 2300;
-            SKImageInfo imageSize = new SKImageInfo(uniqueIDs.Count == 0 ? backgroundWidth : backgroundWidth + 400, 2500);
+            System.Drawing.Size imageSize = new System.Drawing.Size(uniqueIDs.Count == 0 ? backgroundWidth : backgroundWidth + 400, 2500);
             Console.WriteLine("GenerateScheduleImage - 5");
-            using (SKSurface surface = SKSurface.Create(imageSize))
+            using (Bitmap bm = new Bitmap(imageSize.Width, imageSize.Height))
             {
-                Console.WriteLine("GenerateScheduleImage - 6");
-                SKCanvas canvas = surface.Canvas;
-                canvas.Clear();
-                canvas.DrawRect(0, 0, 200, 200, userColours[uniqueIDs[0]]);
-                Console.WriteLine("GenerateScheduleImage - 7");
-                // save the file
-                SKData data = surface.Snapshot().Encode(SKEncodedImageFormat.Png, 100);
-                Console.WriteLine("GenerateScheduleImage - 8");
-                var stream = File.OpenWrite("schedule.png");
-                Console.WriteLine("GenerateScheduleImage - 9");
-                data.SaveTo(stream);
-                Console.WriteLine("GenerateScheduleImage - 10");
+                // Create a Graphics object from the bitmap
+                using (Graphics graphics = Graphics.FromImage(bm))
+                {
+                    // Set the background color to white
+                    graphics.Clear(System.Drawing.Color.White);
+
+                    // Draw a semi-transparent red rectangle
+                    using (Brush redBrush = new SolidBrush(System.Drawing.Color.FromArgb(128, 255, 0, 0))) // Alpha = 128
+                    {
+                        graphics.FillRectangle(redBrush, 100, 100, 200, 150);
+                    }
+
+                    // Draw a blue line
+                    using (Pen bluePen = new Pen(System.Drawing.Color.Blue, 5)) // Width = 5
+                    {
+                        graphics.DrawLine(bluePen, 50, 50, 300, 300);
+                    }
+
+                    // Draw some text
+                    using (Font font = new Font("Arial", 24))
+                    using (Brush textBrush = new SolidBrush(System.Drawing.Color.Black))
+                    {
+                        graphics.DrawString("Hello, World!", font, textBrush, new PointF(50, 400));
+                    }
+                }
+                bm.Save("schedule.png", System.Drawing.Imaging.ImageFormat.Png);
             }
+            //using (SKSurface surface = SKSurface.Create(imageSize))
+            //{
+            //    Console.WriteLine("GenerateScheduleImage - 6");
+            //    SKCanvas canvas = surface.Canvas;
+            //    canvas.Clear();
+            //    canvas.DrawRect(0, 0, 200, 200, userColours[uniqueIDs[0]]);
+            //    Console.WriteLine("GenerateScheduleImage - 7");
+            //    // save the file
+            //    SKData data = surface.Snapshot().Encode(SKEncodedImageFormat.Png, 100);
+            //    Console.WriteLine("GenerateScheduleImage - 8");
+            //    var stream = File.OpenWrite("schedule.png");
+            //    Console.WriteLine("GenerateScheduleImage - 9");
+            //    data.SaveTo(stream);
+            //    Console.WriteLine("GenerateScheduleImage - 10");
+            //}
         }
 
         /// <summary>
@@ -187,10 +213,11 @@ namespace CronoCord.Modules
         /// </summary>
         /// <param name="userIDs">List of user ids to get colours for</param>
         /// <returns>Dictionary with user id as key and color as value</returns>
-        private Dictionary<ulong, SKPaint> GenerateUserColours(List<ulong> userIDs)
+        private Dictionary<ulong, System.Drawing.Color> GenerateUserColours(List<ulong> userIDs)
         {
+            Console.WriteLine("GenerateUserColours - 1");
             // Generate a "light" colour
-            SKPaint GenerateColour()
+            System.Drawing.Color GenerateColour()
             {
                 while (true)
                 {
@@ -200,22 +227,32 @@ namespace CronoCord.Modules
                     // Avoid colors with too similar RGB values (to skip gray-like tones)
                     // Avoid brownish hues by ensuring no dominant mix of red and green
                     if (Math.Abs(r - g) > 30 || Math.Abs(r - b) > 30 || Math.Abs(g - b) > 30 && !(r > 200 && 210 < g && g > 150 && b < 100))
-                        return new SKPaint
-                        {
-                            Color = new SKColor(r, g, b, 128),
-                            IsAntialias = true
-                        };
+                        return System.Drawing.Color.FromArgb (128, r, g, b);
                 }
             }
-            Dictionary<ulong, SKPaint> userColours = new Dictionary<ulong, SKPaint>();
+            Dictionary<ulong, System.Drawing.Color> userColours = new Dictionary<ulong, System.Drawing.Color>();
             // Generate unique colours for each user
-            foreach (ulong userID in userIDs)
+
+            try
             {
-                SKPaint possibleColour = null;
-                do possibleColour = GenerateColour();
-                while (!userColours.ContainsValue(possibleColour));
-                userColours.Add(userID, possibleColour);
+                foreach (ulong userID in userIDs)
+                {
+                    Console.WriteLine("GenerateUserColours - 2");
+                    System.Drawing.Color possibleColour = System.Drawing.Color.Empty;
+                    do possibleColour = GenerateColour();
+                    while (userColours.ContainsValue(possibleColour));
+                    Console.WriteLine("GenerateUserColours - 3");
+                    userColours.Add(userID, possibleColour);
+                    Console.WriteLine("GenerateUserColours - 4");
+                }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Gen Col Error: {ex}");
+            }
+            
+
+            Console.WriteLine("GenerateUserColours - 5");
 
             return userColours;
         }
