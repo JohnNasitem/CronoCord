5521548883c3ea847e6885a6de3b462723f25eca
JohnNasitem <jc.nasitem@gmail.com>
2025-02-22 15:50:28 -0800

    wrapped commnads in a try catch

--- CronoCord/Modules/DatabaseModule.cs
+++ CronoCord/Modules/DatabaseModule.cs
@@ -28,12 +28,20 @@ namespace CronoCord.Modules
         [SlashCommand("reset-database", "Reset a database table")]
         public async Task ResetDatabaseCommand(DatabaseManagement.DatabaseTables table)
         {
-            bool success = false;
-            await Task.Run(() => success = DatabaseManagement.ResetTable(table));
-            if (success)
-                await RespondAsync($"Successfully reset database", ephemeral: true);
-            else
-                await RespondAsync($"Error, Couldn't reset database", ephemeral: true);
+            try
+            {
+                bool success = false;
+                await Task.Run(() => success = DatabaseManagement.ResetTable(table));
+                if (success)
+                    await RespondAsync($"Successfully reset database", ephemeral: true);
+                else
+                    await RespondAsync($"Error, Couldn't reset database", ephemeral: true);
+            }
+            catch (Exception ex)
+            {
+                await RespondAsync($"Something went wrong... contact <@{Program.AuthorID}>", ephemeral: true);
+                UtilityMethods.PrettyConsoleWriteLine($"Problem occured in ResetDatabaseCommand(). Exception: {ex}", UtilityMethods.LogLevel.Error);
+            }
         }
 
 
@@ -46,23 +54,31 @@ namespace CronoCord.Modules
         [SlashCommand("clean-up-database", "Removes old entries in the specified table")]
         public async Task CleanUpDatabaseCommand(DatabaseManagement.DatabaseTables table)
         {
-            List<int> deletedEntries = new List<int>();
-            long sundayUnix = UtilityMethods.GetSundayUnixTimeStamp(true);
+            try
+            {
+                List<int> deletedEntries = new List<int>();
+                long sundayUnix = UtilityMethods.GetSundayUnixTimeStamp(true);
 
-            // Clean up availabilites table
-            if (table == DatabaseManagement.DatabaseTables.Availabilities || table == DatabaseManagement.DatabaseTables.All)
-                await Task.Run(() => deletedEntries.Add(DatabaseManagement.CleanUpAvailabilities(sundayUnix)));
+                // Clean up availabilites table
+                if (table == DatabaseManagement.DatabaseTables.Availabilities || table == DatabaseManagement.DatabaseTables.All)
+                    await Task.Run(() => deletedEntries.Add(DatabaseManagement.CleanUpAvailabilities(sundayUnix)));
 
-            string message = "";
+                string message = "";
 
-            if(deletedEntries.Count == 0)
-                message = "Specified table doesnt seem to have any clean up implemented";
-            else if (deletedEntries.Any(i => i == -1))
-                message = "Error, One of the specified databases couldn't be cleaned up";
-            else
-                message = $"Successfully cleaned up database(s) - Removed entries: {deletedEntries.Sum()}";
+                if (deletedEntries.Count == 0)
+                    message = "Specified table doesnt seem to have any clean up implemented";
+                else if (deletedEntries.Any(i => i == -1))
+                    message = "Error, One of the specified databases couldn't be cleaned up";
+                else
+                    message = $"Successfully cleaned up database(s) - Removed entries: {deletedEntries.Sum()}";
 
-            await RespondAsync(message, ephemeral: true);
+                await RespondAsync(message, ephemeral: true);
+            }
+            catch (Exception ex)
+            {
+                await RespondAsync($"Something went wrong... contact <@{Program.AuthorID}>", ephemeral: true);
+                UtilityMethods.PrettyConsoleWriteLine($"Problem occured in CleanUpDatabaseCommand(). Exception: {ex}", UtilityMethods.LogLevel.Error);
+            }
         }
     }
 }
