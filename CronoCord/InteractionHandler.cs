3426aeec8452724a523b4ea8dc8f671b73a49b85
JohnNasitem <jc.nasitem@gmail.com>
2025-02-22 17:45:07 -0800

    added more cases to handler interaction execute

--- CronoCord/InteractionHandler.cs
+++ CronoCord/InteractionHandler.cs
@@ -124,6 +124,7 @@ namespace CronoCord
                             await interaction.RespondAsync("You do not meet the preconditions to run this command.");
                             break;
                         default:
+                            Console.WriteLine("HandleInteraction -1");
                             break;
                     }
             }
@@ -146,7 +147,11 @@ namespace CronoCord
                         // This can be due to user permissions, missing arguments, or other conditions
                         context.Interaction.RespondAsync("You do not meet the preconditions to execute this command.");
                         break;
+                    case InteractionCommandError.Exception:
+                        UtilityMethods.PrettyConsoleWriteLine($"Exception occured in {commandInfo.Name}. Exception: {result.ErrorReason}", UtilityMethods.LogLevel.Error);
+                        break;
                     default:
+                        UtilityMethods.PrettyConsoleWriteLine($"Unknown problem occured in during an interaction execute: Error: {result.Error} - {result.ErrorReason}.", UtilityMethods.LogLevel.Error);
                         break;
                 }
 
